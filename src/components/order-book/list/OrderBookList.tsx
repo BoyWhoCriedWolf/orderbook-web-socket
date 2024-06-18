428907023d1fd7028abb54360950d4bfcdea2800
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-06-19 04:28:35 +0800

    fix: subscription

--- src/components/order-book/list/OrderBookList.tsx
+++ src/components/order-book/list/OrderBookList.tsx
@@ -1,17 +1,26 @@
-import React, { useEffect } from "react";
+import {
+  SubscribedContext,
+  SubscribingContext,
+  SubscriptionStateContext,
+} from "centrifuge";
+import { useEffect } from "react";
 import centrifuge from "../../../services";
 const OrderBookList = () => {
   const init = () => {
-    const orderbookSubscription = centrifuge.newSubscription("orderbook:BTC-USD", {
-      getData: async (ctx) => {
-        console.log(ctx);
-        return new Promise<any>((resolve, reject) => {
-          resolve(ctx);
-        });
-      },
-    });
+    const orderbookSubscription =
+      centrifuge.newSubscription("orderbook:BTC-USD");
 
     orderbookSubscription.subscribe();
+
+    orderbookSubscription.on("state", (ctx: SubscriptionStateContext) => {
+      console.log("state", ctx);
+    });
+    orderbookSubscription.on("subscribed", (ctx: SubscribedContext) => {
+      console.log("subscribed", ctx);
+    });
+    orderbookSubscription.on("subscribing", (ctx: SubscribingContext) => {
+      console.log("subscribing", ctx);
+    });
   };
 
   useEffect(() => {
