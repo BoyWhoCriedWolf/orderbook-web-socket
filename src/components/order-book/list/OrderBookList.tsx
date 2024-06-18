53650b7837f9aca542d37429c5b0797e8965489b
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-06-19 04:00:24 +0800

    debug: subscription

--- src/components/order-book/list/OrderBookList.tsx
+++ src/components/order-book/list/OrderBookList.tsx
@@ -1,29 +1,24 @@
-import { Centrifuge } from "centrifuge";
 import React, { useEffect } from "react";
-import { WEB_SOCKET_API } from "../../../services/api-urls";
-import { API_KEYS } from "../../../services/api-config";
-
-const centrifuge = new Centrifuge(WEB_SOCKET_API.MAIN_NET);
-
-centrifuge.setToken(API_KEYS.PRIVATE_JWT);
-
-centrifuge.connect();
-
-centrifuge.on("connected", function (context) {
-  console.log("Connected to Centrifuge", context);
-});
-
-centrifuge.on("disconnected", function (context) {
-  console.log("Disconnected from Centrifuge", context);
-});
-
-centrifuge.on("error", function (err) {
-  console.error("Centrifuge error:", err);
-});
-centrifuge.newSubscription("orderbook", {});
-
+import centrifuge from "../../../services";
 const OrderBookList = () => {
-  const init = () => {};
+  const init = () => {
+    centrifuge.newSubscription("orderbook", {
+      getData: async (ctx) => {
+        console.log(ctx);
+        return new Promise<any>((resolve, reject) => {
+          resolve(ctx);
+        });
+      },
+    });
+    centrifuge.newSubscription("orderbook:BTC-USD", {
+      getData: async (ctx) => {
+        console.log(ctx);
+        return new Promise<any>((resolve, reject) => {
+          resolve(ctx);
+        });
+      },
+    });
+  };
 
   useEffect(() => {
     init();
