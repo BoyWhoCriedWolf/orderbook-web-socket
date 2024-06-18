7dfadf76e7df601c518d0ac73fcde9bd115c20ed
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-06-19 02:35:14 +0800

    debug: connect to server

--- src/services/api-config.ts
+++ src/services/api-config.ts
@@ -1,1 +1,4 @@
-export const API_KEYS = {};
+export const API_KEYS = {
+  PRIVATE_JWT:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3MzY4OCIsImV4cCI6MTcyNjUwNzQwMn0.oOZV-92D_cVtaQt6MQpvm-ysjcSlDgH7HF_T0f_iYqo",
+};
--- src/services/centrifuge.service.ts
+++ src/services/centrifuge.service.ts
@@ -1,25 +1,19 @@
 import { Centrifuge } from "centrifuge";
 import { WEB_SOCKET_API } from "./api-urls";
+import { API_KEYS } from "./api-config";
 
 const centrifuge = new Centrifuge(WEB_SOCKET_API.TEST_NET);
 
-// Set JWT token for authentication
-// const jwtToken = process.env.PRIVATE_JWT ?? "";
-
-console.log("env=>", process.env);
-
-centrifuge.setToken(
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3MzY4OCIsImV4cCI6MTcyNjUwNzQwMn0.oOZV-92D_cVtaQt6MQpvm-ysjcSlDgH7HF_T0f_iYqo"
-);
+centrifuge.setToken(API_KEYS.PRIVATE_JWT);
 
 centrifuge.connect();
 
 centrifuge.on("connected", function (context) {
-  console.log("Connected to Centrifuge");
+  console.log("Connected to Centrifuge", context);
 });
 
 centrifuge.on("disconnected", function (context) {
-  console.log("Disconnected from Centrifuge");
+  console.log("Disconnected from Centrifuge", context);
 });
 
 centrifuge.on("error", function (err) {
