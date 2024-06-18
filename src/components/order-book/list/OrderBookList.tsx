8e5c8489421d85e07075cd7447ccf93269c6e1da
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-06-19 02:46:34 +0800

    fix: jwt private token

--- src/components/order-book/list/OrderBookList.tsx
+++ src/components/order-book/list/OrderBookList.tsx
@@ -1,12 +1,11 @@
 import { Centrifuge } from "centrifuge";
 import React, { useEffect } from "react";
 import { WEB_SOCKET_API } from "../../../services/api-urls";
+import { API_KEYS } from "../../../services/api-config";
 
 const centrifuge = new Centrifuge(WEB_SOCKET_API.TEST_NET);
 
-centrifuge.setToken(
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0MDAwMDAwMDAwIiwiZXhwIjo2NTQ4NDg3NTY5fQ.o_qBZltZdDHBH3zHPQkcRhVBQCtejIuyq8V1yj5kYq8"
-);
+centrifuge.setToken(API_KEYS.PRIVATE_JWT);
 
 centrifuge.connect();
 
