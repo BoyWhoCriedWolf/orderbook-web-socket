1b96ed028ace5c88a6e7a23e33f186561db80415
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-06-18 23:47:50 +0800

    feat: basic style for bocy

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import "./App.css";
 import OrderBookList from "./components/order-book/list";
 
 function App() {
-  return <OrderBookList />;
+  return (
+    <div className="bg-slate-950 text-white">
+      <OrderBookList />;
+    </div>
+  );
 }
 
 export default App;
