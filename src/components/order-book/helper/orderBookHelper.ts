fa5ca60a9801eab5e6d21713f0b51e4a598d7de0
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-06-19 17:41:56 +0800

    fix: calculation

--- src/components/order-book/helper/orderBookHelper.ts
+++ src/components/order-book/helper/orderBookHelper.ts
@@ -6,14 +6,14 @@ export const calculateOrderBookData = (
 
   const calculatedData = data
     .sort((a, b) => parseFloat(a?.[0]) - parseFloat(b?.[0]))
+    .filter((item) => {
+      const [price, amount] = item;
+      return parseFloat(`${amount}`) > 0;
+    })
     .map((item, itemIndex) => {
       const [price, amount] = item;
       t += parseFloat(amount);
       return [price, amount, t];
-    })
-    .filter((item) => {
-      const [price, amount] = item;
-      return parseFloat(`${amount}`) > 0;
     });
   // .slice(-10);
 
