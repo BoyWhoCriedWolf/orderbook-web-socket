8f16044c33b740be5d1b8a562defac52932a9cbe
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-06-19 13:28:39 +0800

    fix: decimal formatter sigit

--- src/utils/number-utils.ts
+++ src/utils/number-utils.ts
@@ -1,4 +1,4 @@
-export const decimalFormatter = (p?: number | string, decimalDigit = 2) => {
+export const decimalFormatter = (p?: number | string, decimalDigit = 4) => {
   const formatter = new Intl.NumberFormat("en-US", {
     minimumFractionDigits: decimalDigit,
   });
